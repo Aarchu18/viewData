3365def44b48919e50f78946c2df32d923a08025
Archana <archana.kumari@kelltontech.com>
2018-11-08 13:08:09 +0530

    first commit

---
+++ ViewData/Controllers/DetailsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ViewData.Models.Details;
+
+namespace ViewData.Controllers
+{
+    public class DetailsController : Controller
+    {
+
+    public IActionResult Index()
+        {
+            ViewData["Name"] = "archana";
+            ViewData["Phone"] = "Mi";
+            ViewData["Address"] = "jaipur";
+            ViewData["Interest"] = "Reading";
+
+            DataClass dataclass = new DataClass
+            {
+                Count = ViewData.Count,
+                key = ViewData.Keys,
+                Value = ViewData.Values
+
+            };
+
+            return View(dataclass);
+        }
+    }
+}
---
+++ ViewData/Models/Details/DataClass.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ViewData.Models.Details
+{
+    public class DataClass
+    {
+        public int Count { get; set; }
+
+        public ICollection<string> key { get; set; }
+
+        public ICollection<object> Value { get; set; }
+    }
+}
